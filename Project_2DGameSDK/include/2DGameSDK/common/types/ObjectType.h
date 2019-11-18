626c4f07e686e13eed5dc99d19185f7e5c75a1bc
Aleistar Markóczy <a.markoczy@gmail.com>
2019-11-18 22:04:38 +0100

    objecttypes

---
+++ Project_2DGameSDK/include/2DGameSDK/common/types/ObjectType.h
@@ -0,0 +1,12 @@
+#ifndef __OBJECT_TYPE_H__
+#define __OBJECT_TYPE_H__
+
+namespace game {
+  enum class ObjectType {
+    Unknown = 0,
+    Tile = 1,
+    Entity = 2
+  };
+} // namespace game
+
+#endif
