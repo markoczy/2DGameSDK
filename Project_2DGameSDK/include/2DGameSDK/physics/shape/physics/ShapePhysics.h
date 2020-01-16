9f0cb88d040c4f4563a26a0336e428ba8c831883
Aleistar Markóczy <a.markoczy@gmail.com>
2020-01-16 16:18:07 +0100

    move shapephysics to game ns

--- Project_2DGameSDK/include/2DGameSDK/physics/shape/physics/ShapePhysics.h
+++ Project_2DGameSDK/include/2DGameSDK/physics/shape/physics/ShapePhysics.h
@@ -14,29 +14,33 @@
 #include <2DGameSDK/dll/gamesdk_dll.h>
 #include <chipmunk/chipmunk.h>
 
-/**
+namespace game {
+
+  /**
  * @brief Generic Shape Physics strategy
  * 
  * @tparam TDefinition the concrete type (used for copy)
  */
-template <class TDefinition>
-class GAMESDK_DLL ShapePhysics {
-public:
-  /**
+  template <class TDefinition>
+  class GAMESDK_DLL ShapePhysics {
+  public:
+    /**
    * @brief Initializes the chipmunk shape
    * 
    * @param space the chipmunk space
    * @param body the chipmunk body
    * @param shape the chipmunk shape
    */
-  virtual void InitProperties(cpSpace* space, cpBody* body, cpShape* shape) = 0;
+    virtual void InitProperties(cpSpace* space, cpBody* body, cpShape* shape) = 0;
 
-  /**
+    /**
    * @brief Creates a copy of the shape physics
    * 
    * @return TDefinition* the copy
    */
-  virtual TDefinition* Copy() = 0;
-};
+    virtual TDefinition* Copy() = 0;
+  };
+
+} // namespace game
 
 #endif
