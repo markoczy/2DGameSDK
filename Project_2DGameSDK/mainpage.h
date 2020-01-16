950ffae32889cb899df48c6e4994e54710f1cced
markoczy <a.markoczy@gmail.com>
2020-01-16 18:26:47 +0100

    updated mainpage

--- Project_2DGameSDK/mainpage.h
+++ Project_2DGameSDK/mainpage.h
@@ -3,7 +3,7 @@
  * @author Aleistar Markoczy (a.markoczy@gmail.com)
  * @brief Mainpage of doxygen documentation
  * @version 1.0
- * @date 2019-06-12
+ * @date 2020-01-16
  * 
  * @copyright Copyright (c) 2020
  * 
@@ -24,6 +24,7 @@
  * 
  * ```
  * 
+ * // File: minimal_demo.cpp
  * #include <2DGameSDK/Core.h>
  * #include <2DGameSDK/Scene.h>
  * #include <2DGameSDK/World.h>
@@ -34,19 +35,21 @@
  * // Type of player entity
  * const int _PLAYER_TYPE = 200;
  * 
+ * // Make an Entity that rotates on the Screen
  * class RotatingEntity : public SpriteKinematicEntity {
  * public:
- *   RotatingEntity(sf::Texture* texture,
+ *   RotatingEntity(Game* game,
+ *                  sf::Texture* texture,
  *                  float rotPerTick,
- *                  sf::Vector2f pos = sf::Vector2f()) : SpriteKinematicEntity(1, texture), mRot(rotPerTick) {
- *     auto rect = mSprite.getTextureRect();
- *     // Set origin to center of the Texture
- *     GetTransformable()->setOrigin(float(rect.width) / 2.0, float(rect.height) / 2.0);
- *     GetTransformable()->setPosition(pos);
+ *                  sf::Vector2f pos = sf::Vector2f()) : SpriteKinematicEntity(1, game, texture), mRot(rotPerTick) {
+ * 
+ *     SetTransform(sf::Transform().translate(pos));
  *   }
  * 
  *   void OnTick() {
- *     GetTransformable()->rotate(mRot);
+ *     Transform(sf::Transform().rotate(mRot));
+ *     auto pt = mTransform.transformPoint(sf::Vector2f());
+ *     pt = mCombinedTransform.transformPoint(sf::Vector2f());
  *   }
  * 
  * private:
@@ -56,25 +59,29 @@
  * int minimalDemo() {
  *   cout << "Start minimalDemo" << endl;
  * 
+ *   // Create game
+ *   auto options = GameOptions{"My Game", sf::Vector2i(512, 512), 5.0, 50};
+ *   auto game = new Game(options);
+ * 
  *   // Create Game World
- *   auto world = GameWorldFactory::CreateGameWorld("res/simple_grass/tilemap.json", "", "res/simple_grass/tile_");
+ *   auto world = GameWorldFactory::CreateGameWorld(game, "res/maps/simple_grass/tilemap.json", "", "res/maps/simple_grass/tile_");
+ *   game->SetWorld(world);
+ *   cout << "GameWorld created." << endl;
  * 
  *   // Create Player entity and Rotating child entity
  *   auto tex = AssetManager::GetTexture("res/textures/discus.png");
- *   auto ent = new RotatingEntity(tex, 5.0, sf::Vector2f(50, 50));
- *   auto ent2 = new RotatingEntity(tex, 10.0);
+ *   auto ent = new RotatingEntity(game, tex, 5.0, sf::Vector2f(50, 50));
  * 
  *   // Layout entities in scene
  *   auto scene = new SceneGraph(game);
- *   auto parent = scene->GetRoot()->AddChild(ent);
- *   auto child = parent->AddChild(ent2);
- * 
- *   // Create game
- *   GameOptions options{"My Game", sf::Vector2i(512, 512), 5.0, 50};
- *   auto app = new Game(options, scene, world);
+ *   auto parent = scene->AddEntity(ent);
+ *   scene->AddEntity(ent, parent);
+ *   game->SetScene(scene);
  * 
  *   // Run Game
- *   app->Run();
+ *   game->Run();
+ * 
+ *   return 0;
  * }
  *
  * ```
